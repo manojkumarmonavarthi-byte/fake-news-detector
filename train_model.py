95c5136165668fbcb3b3c658e40affcc0c035742
Manoj Kumar <manojkumarmonavarthi@gmail.com>
2026-05-07 16:58:37 +0530

    trained high accuracy model

--- train_model.py
+++ train_model.py
@@ -1,87 +1,37 @@
 import pandas as pd
-import pickle
-
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-
-# ---------------- LOAD DATA ----------------
+import joblib
 
 fake = pd.read_csv("Fake.csv")
-real = pd.read_csv("True.csv")
-
-# ---------------- ADD LABELS ----------------
+true = pd.read_csv("True.csv")
 
 fake["label"] = 0
-real["label"] = 1
+true["label"] = 1
 
-# ---------------- COMBINE DATA ----------------
+data = pd.concat([fake, true])
 
-data = pd.concat([fake, real])
-
-# ---------------- KEEP ONLY REQUIRED COLUMNS ----------------
-
-data = data[["text", "label"]]
-
-# ---------------- REMOVE NULL VALUES ----------------
-
-data.dropna(inplace=True)
-
-# ---------------- SPLIT DATA ----------------
-
-X = data["text"]
+x = data["text"]
 y = data["label"]
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size=0.2,
-    random_state=42
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=42
 )
 
-# ---------------- TEXT VECTORIZATION ----------------
+vectorizer = TfidfVectorizer(stop_words="english", max_df=0.7)
 
-vectorizer = TfidfVectorizer(
-    stop_words="english",
-    max_df=0.7
-)
-
-X_train_vectorized = vectorizer.fit_transform(X_train)
-X_test_vectorized = vectorizer.transform(X_test)
-
-# ---------------- MODEL TRAINING ----------------
+xv_train = vectorizer.fit_transform(x_train)
+xv_test = vectorizer.transform(x_test)
 
 model = LogisticRegression()
 
-model.fit(
-    X_train_vectorized,
-    y_train
-)
+model.fit(xv_train, y_train)
 
-# ---------------- MODEL EVALUATION ----------------
+pred = model.predict(xv_test)
 
-y_pred = model.predict(
-    X_test_vectorized
-)
+print("Accuracy:", accuracy_score(y_test, pred))
 
-accuracy = accuracy_score(
-    y_test,
-    y_pred
-)
-
-print(f"Model Accuracy: {accuracy * 100:.2f}%")
-
-# ---------------- SAVE MODEL ----------------
-
-pickle.dump(
-    model,
-    open("model.pkl", "wb")
-)
-
-pickle.dump(
-    vectorizer,
-    open("vectorizer.pkl", "wb")
-)
-
-print("Model Trained Successfully")
+joblib.dump(model, "model.pkl")
+joblib.dump(vectorizer, "vectorizer.pkl")
